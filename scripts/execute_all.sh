44543fce7d03498e0ee9ff00c3a835d08bdce614
DomenicoAmato01 <domenico.amato01@unipa.it>
2022-01-04 11:40:44 +0100

    Deleted unnecessary files

--- scripts/execute_all.sh
+++ scripts/execute_all.sh
@@ -1,11 +1,5 @@
 scripts/execute.sh -c
-#scripts/execute_linear.sh
-# scripts/execute_interpolation.sh -c
 scripts/execute_branchless.sh -c
 scripts/execute_kbbs.sh -c
 scripts/execute_kbfs.sh -c
 scripts/execute_eytzinger.sh -c
-#scripts/execute_log2.sh
-#scripts/execute_fence.sh
-#scripts/execute_build.sh
-#scripts/execute_perf.sh
--- scripts/rebuild_rmis.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-rm -f competitors/rmi/*.h
-rm -f competitors/rmi/*.cpp
-rm -f rmi_data/*
-
-scripts/build_rmis.sh
